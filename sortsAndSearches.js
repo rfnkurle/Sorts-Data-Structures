0163e41c14867b7ad56df73377aa761535a3e0cd
robiwankenobi <rfnkurle@gmail.com>
2019-10-07 21:20:21 -0700

    added linear and binary search

--- sortsAndSearches.js
+++ sortsAndSearches.js
@@ -41,6 +41,56 @@
 
 
 
+    // function insertionSort(arr){
+//     for(var i = 0; i < arr.length; i++){
+//         var current = arr[i];
+//         for(var j = i -1; j >= 0 && arr[j] > current]; j--){ //keep going if j is greater than 0 AND arr[j] is greater than current value
+//             arr[j +1] = arr[j]
+//         }
+//         arr[j+1] = current 
+//     }
+//     return arr;
+// }
+
+// insertionSort([34,22,10,19,17])
+
+
+
+// function linearSearch(arr, b){
+//   for(var i = 0; i <= arr.length; i++){
+//     if (arr[i] === b) return i;
+//   }
+//   return -1;
+// }
+
+// linearSearch([1,2,3,4], 3)
+
+
+
+
+// function binarySearch(arr, a){
+// var start = arr[0];
+// var end = arr.length -1;
+// var middle = Math.floor((start + end) / 2)
+// while (arr[middle] !== a && start <= end){
+//     if (a < arr[middle]){
+//         end = middle -1;
+//     }else{
+//         start = middle +1
+//     }
+//     middle = Math.floor((start+end) /2);
+// }
+// return arr[middle] === a ? middle : -1
+
+// }
+
+// binarySearch([2,3,5,6,9,13,15,28], 15)
+
+
+
+
+
+
 
 //add algorithm/function and arguments
 document.getElementById("example").innerHTML = mergeSort([2, 5, 1, 7, 3, 9])
