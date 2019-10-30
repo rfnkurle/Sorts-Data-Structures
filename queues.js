ae5fd8b3e91a1fa51b73ef82f23f80ec425528d7
robiwankenobi <rfnkurle@gmail.com>
2019-10-30 15:06:47 -0700

    added queues

---
+++ queues.js
@@ -0,0 +1,41 @@
+//remove from front of line with shift
+//better to write your own Q class if you are concerned with performance
+
+class Node{
+    constructor(){
+        this.value = val;
+        this.next= null;
+        this.size = 0;
+
+    }
+}
+
+class Queue{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    enqueue(val){
+        var newNode = newNode(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode; 
+        } else {
+            this.last.next = newNode; //end
+            this.last = newNode;//now ^ set to last
+
+        }
+        return ++this.size
+    }
+    dequeue(){
+        if(!this.first) return null;
+        var temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
